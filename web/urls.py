e41a8082ae464b0d42e39a1ac66874ed25015e08
liguojun518@63.com <ligj41612>
2019-12-07 23:26:08 +0800

    列表页和详情页模板修改
    Signed-off-by: liguojun518@63.com <ligj41612>

--- web/urls.py
+++ web/urls.py
@@ -2,5 +2,10 @@ from . import views
 from django.conf.urls import url
 
 urlpatterns = [
-    url('index',views.index,name='index')
+    url('^/?$',views.index),
+    url('^/index$',views.index,name='index'),
+    url('^/list/(\d+)?/?(\d+)?$',views.list,name="list"),  #第一个括号为分类，第二个括号为页码
+    url('^/detail/(\d+)$',views.detail,name='detail')
 ]
+
+# 大分类_小分类_品牌_页码
--- web/views.py
+++ web/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,"index.html")
+
+def list(request,cat,pages=1):
+    print('cat:{0},pages={1}'.format(cat,pages))
+    return render(request,"list.html")
+
+def detail(request,id):
+    return render(request,"detail.html")
